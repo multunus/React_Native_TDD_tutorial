538477a64dc1dd52dbab6983f067b95366421bde
Jk <jayakrishnanmk07@gmail.com>
2016-05-16 12:57:09 +0530

    Fix app naming bug

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react-native';
-import CommentBox from './src/components/CommentBox.js';
+import CommentBox from './components/CommentBox.js';
 
 
 export default class App extends Component {
