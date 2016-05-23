ca1b96c58be6d68210c6860e51c022d6b2145403
Jk <jayakrishnanmk07@gmail.com>
2016-05-23 16:09:25 +0530

    Refactor commentlist specs

--- src/components/__specs__/CommentList.spec.js
+++ src/components/__specs__/CommentList.spec.js
@@ -6,22 +6,20 @@ import CommentList from '../CommentList.js';
 import Comment from '../Comment.js';
 
 describe('<CommentList />', () => {
-  it('should render 1 view components', () => {
-       var data = [
-         {id: 1, author: "Pete Hunt", text: "This is one comment"},
-         {id: 2, author: "Jordan Walke", text: "This is a super comment"},
-         {id: 2, author: "Jordan Walke", text: "This is an ordinary comment"}
+  beforeEach(function() {
+    data = [
+      {id: 1, author: "Pete Hunt", text: "This is one comment"},
+      {id: 2, author: "Jordan Walke", text: "This is a super comment"},
+      {id: 3, author: "Jordan Walkerr", text: "This is an ordinary comment"}
     ];
+  });
+
+  it('should render 1 view components', () => {
     const wrapper = shallow(<CommentList data={data} />);
     expect(wrapper.find(View)).to.have.length(1);
   });
 
   it('should render comment components with correct comment text', () => {
-    var data = [
-      {id: 1, author: "Pete Hunt", text: "This is one comment"},
-      {id: 2, author: "Jordan Walke", text: "This is a super comment"},
-      {id: 2, author: "Jordan Walke", text: "This is an ordinary comment"}
-    ];
     const wrapper = shallow(<CommentList data={data}/>);
     for(var index in data) {
       comment = data[index];
@@ -30,11 +28,6 @@ describe('<CommentList />', () => {
   });
 
   it('should render comment components with correct props for each entry in data array', () => {
-    var data = [
-      {id: 1, author: "Pete Hunt", text: "This is one comment"},
-      {id: 2, author: "Jordan Walke", text: "This is a super comment"},
-      {id: 3, author: "Jordan Walkerrr", text: "This is an ordinary comment"}
-    ];
     const wrapper = shallow(<CommentList data={data} />);
     expect(wrapper.find(Comment)).to.have.length(3);
     for(var index in data) {
